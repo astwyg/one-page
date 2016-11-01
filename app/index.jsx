906f9da289a5d64ce684c47adc435cd9bb2a64e6
WangYG <wangyonggang@ctyun.cn>
2016-11-01 20:31:12 +0800

    修正一些小问题

--- app/index.jsx
+++ app/index.jsx
@@ -4,19 +4,24 @@ import {AvgGrid, Button, Dropdown, ModalTrigger, Modal} from 'amazeui-react';
 
 const styles = {
   mainBlock:{
-    padding:"200px 50px 0 50px",
+    padding:"20px 50px 0 50px",
+  },
+  title:{
+    padding:"40px 50px 0 50px",
+    textAlign:"center",
   }
 }
 
 const modalInfo = <Modal title="还没搞过来">旧的WorldPress站点正在搬移中.</Modal>;
 const modalPay = <Modal title="向我付款"><div><p>左边支付宝, 右边微信, 请先加好友再转账.</p><img src="/images/alipay.png" width="50%" /><img src="/images/wechat.jpg" width="50%" /></div></Modal>;
 
-const ThisPage = (
+const ThisPage = (<div>
+  <div style={styles.title}><h2>静态站, 就剩下面几个功能了.</h2></div>
   <AvgGrid sm={1} md={3} lg={3} className="am-thumbnails" style={styles.mainBlock}>
     <li>
       <Button amStyle="primary" block onClick={()=>window.open("http://blog.leanote.com/astwyg@outlook.com")}>笔记本</Button>
     </li>
-    <li><ModalTrigger modal={modalInfo}><Button amStyle="default" block>旧博客</Button></ModalTrigger></li>
+    <li><Button amStyle="default" block onClick={()=>window.open("/archives/2")}>旧博客</Button></li>
     <li><ModalTrigger modal={modalPay}><Button amStyle="secondary" block>向我付款</Button></ModalTrigger></li>
     <li><Button amStyle="success" block onClick={()=>window.open("https://outlook.live.com/owa//calendar/8d120968-ff3c-488e-a036-2d0056de74cb/8888d70a-7b67-4096-9ed9-a1d732e362e4/cid-820FF0510D3252E0/index.html")}>查看日程</Button></li>
     <li>
@@ -32,6 +37,6 @@ const ThisPage = (
       </Dropdown>
     </li>
   </AvgGrid>
-);
+</div>);
 
 ReactDOM.render(ThisPage, document.getElementById('page'));
